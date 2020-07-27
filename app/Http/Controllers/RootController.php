94d0bb875e22db217e9873d2f26bf0c5fdd5b1d7
HowardSun <amzhoward588@gmail.com>
2020-07-27 18:38:21 +0800

    update RootController

--- app/Http/Controllers/RootController.php
+++ app/Http/Controllers/RootController.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class RootController extends Controller
 {
     public function welcome()
     {
         return view('welcome');
     }
+
+    public function infos(Request $request)
+    {
+        return response([
+            'host' => $request->getHost(),
+        ]);
+    }
+    
 }
